db479cd4ca0137d13ee9f73676d0ce69108bd24b
Josipaa <josipa.ruzic@fer.hr>
2021-05-25 23:25:34 +0200

    prvi commit

---
+++ src/main/webapp/precache-manifest.9d7b1f5a0018fa88b978d26a86e0a576.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d568aa96b059e18dfd24c5ee13684a51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdbaadfa07cabd61b6b5",
+    "url": "/static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "6442380d6937d897f86c",
+    "url": "/static/css/main.9b4b4fea.chunk.css"
+  },
+  {
+    "revision": "bdbaadfa07cabd61b6b5",
+    "url": "/static/js/2.abc8fd56.chunk.js"
+  },
+  {
+    "revision": "fe3524a3e01a8fc0a1bfdc318a5768b1",
+    "url": "/static/js/2.abc8fd56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6442380d6937d897f86c",
+    "url": "/static/js/main.8e1580ea.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
